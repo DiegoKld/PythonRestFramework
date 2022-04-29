5a2f2f57cbdc557d7dfcb20aee6b3b95a90dc8dd
Diego Calderon <38056055+DiegoKld@users.noreply.github.com>
2022-04-29 15:58:48 -0500

    Se agrega el url de cada app al urls general del proyecto

--- empleados/empleados/urls.py
+++ empleados/empleados/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #incluimos las urls de la app departamento
+    path('', include('applications.departamento.urls')),
+
+    #incluimos las urls de la app persona
+    path('', include('applications.persona.urls')),
+    
 ]
