3d6c3aba75c2c460ad756d9f6e9ae2ed0ce28ffb
Diego Calderon <38056055+DiegoKld@users.noreply.github.com>
2022-05-03 17:54:32 -0500

    Se genera una tabla para mostrar detalles en empleados del admin

--- empleados/applications/persona/models.py
+++ empleados/applications/persona/models.py
@@ -1,9 +1,20 @@
+from distutils.command.upload import upload
 from django.db import models
 from django.forms import ImageField
 from applications.departamento.models import Departamento
 
 # Create your models here.
 
+class Habilidades(models.Model):
+    habilidad = models.CharField('Habilidad', max_length=50)
+
+    class Meta:
+        verbose_name='Habilidad'
+        verbose_name_plural='Habilidades' 
+
+    def __str__(self):
+        return str(self.id) + ' - ' + self.habilidad
+    
 
 class Empleado(models.Model):
     """ MODELO PARA CADA EMPLEADO """
@@ -12,7 +23,9 @@ class Empleado(models.Model):
         ('0', 'Contador'),
         ('1', 'Administrador'),
         ('2', 'Economista'),
-        ('3', 'Otro'),
+        ('3', 'Operaciones'),
+        ('4', 'Proyectos'),
+        ('5', 'Otro'),
     )
     # contador
     # administrador
@@ -22,7 +35,14 @@ class Empleado(models.Model):
     last_name = models.CharField('Apellidos', max_length=50)
     job = models.CharField('Trabajo', max_length=50, choices=JOB_CHOICES)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    # FIELDNAME = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+    #vatar = models.ImageField(upload_to='empleado', height_field=None, width_field=None, max_length=None)
+    habilidades = models.ManyToManyField(Habilidades)
+
+    class Meta:
+        verbose_name='Personal de la empresa'
+        verbose_name_plural='Personal' 
+        ordering = ['first_name']
+        unique_together = ('first_name', 'last_name')
 
     def __str__(self):
         return str(self.id) + ' - ' + self.first_name + ' - ' + self.last_name
